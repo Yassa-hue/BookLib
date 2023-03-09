e56686f63d93e9dfaac98bca75bfc085bb288220
yassa <yassatamer99@gmail.com>
2023-03-09 17:17:09 +0200

    choices page

---
+++ BookLib/NextStep.cs
@@ -0,0 +1,17 @@
+
+
+namespace BookLib;
+
+
+public static class NextStep
+{
+
+    public static int getNextStep(List<string> nextStepChoices)
+    {
+        // to be implemented
+        return -1;
+    }
+
+}
+
+
