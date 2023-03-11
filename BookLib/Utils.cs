ebf11afc0ef32ad0ab4281c08f3515cc27f80f27
yassa <yassatamer99@gmail.com>
2023-03-11 21:25:56 +0200

    adding page name to context

--- BookLib/Utils.cs
+++ BookLib/Utils.cs
@@ -8,16 +8,23 @@ public class Context
 { 
     public User user { get; set; }
     public SqliteConnection dbConnection { get; set; }
+
+    public string pageName { get; set; }
 }
 
 public static class Utils
 {
-    public static Func<T1, T3> compose<T1, T2, T3>(this Func<T1, T2> f1, Func<T2, T3> f2)
+    public static Func<T1, T3> Compose<T1, T2, T3>(this Func<T1, T2> f1, Func<T2, T3> f2)
     {
         return (x) => f2(f1(x));
     }
     
-    public static Func<T1, T2> composeWithDbConn<T1, T2>(this Func<T1, SqliteConnection, T2> f, SqliteConnection dbConn)
+    public static Func<T3> Compose<T2, T3>(this Func<T2> f1, Func<T2, T3> f2)
+    {
+        return () => f2(f1());
+    }
+    
+    public static Func<T1, T2> ComposeWithDbConn<T1, T2>(this Func<T1, SqliteConnection, T2> f, SqliteConnection dbConn)
     {
         return (x) => f(x, dbConn);
     }
