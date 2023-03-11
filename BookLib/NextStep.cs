242ad81f2bfff8b86869cdadb574c2b5f94b866d
yassa <yassatamer99@gmail.com>
2023-03-11 21:26:51 +0200

    returning page name in next step

--- BookLib/NextStep.cs
+++ BookLib/NextStep.cs
@@ -6,10 +6,12 @@ namespace BookLib;
 public static class NextStep
 {
 
-    public static Action<Context> getNextStep(List<(string pageName, Action<Context> funcDel)> nextStepChoices)
+    public static (string pageName, Action<Context> funcDel) getNextStep(List<(string pageName, Action<Context> funcDel)> nextStepChoices)
     {
         // to be implemented
-        return nextStepChoices[0].funcDel;
+
+
+        return nextStepChoices[0];
     }
 
 }
